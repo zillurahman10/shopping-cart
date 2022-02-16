bf6d5f75d9e0262d180c1ffbedcf9d13205f86a7
zilurahman10 <raihan.brac2019@gmail.com>
2022-02-16 15:27:44 +0600

    total added

---
+++ cart.js
@@ -0,0 +1,50 @@
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    const productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productInput.value = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productInput.value = parseInt(productNumber) - 1;
+    }
+    // update case total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productInput.value * price
+}
+
+function getInputValue() {
+    const productInput = document.getElementById(product + '-number')
+    const productNumber = parseInt(productInput.value)
+    return productNumber;
+}
+
+
+function calculateTotal() {
+    // const phoneInput = document.getElementById('phone-number')
+    // const phoneNumber = parseInt(phoneInput.value);
+    const phoneTotal = getInputValue('phone') * 1219
+    const caseTotal = getInputValue('case') * 59
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    // get on the html
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
+}
+
+
+
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true)
+});
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false)
+});
+
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+});
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+});
